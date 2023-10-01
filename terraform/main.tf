31931e018df36e73fe24027b458c58779927f4af
alandevoteam <alan.ahmad@devoteam.com>
2023-10-01 22:37:00 +0200

    update tf and ansible ports

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,13 +32,13 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "HTTP"
+    name                       = "webapp"
     priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "80"
+    destination_port_range     = "8080"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
